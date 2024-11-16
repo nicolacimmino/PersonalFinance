5ff470ea7660ae775f01776ce1e9403ed77f07d5
Nicola Cimmino <nicola.cimmino@gmail.com>
2024-11-16 13:02:49 +0100

    More alerts

--- Database/migrations/2024-10-24-155100_add_alerts/up.sql
+++ Database/migrations/2024-10-24-155100_add_alerts/up.sql
@@ -22,4 +22,24 @@ SELECT gen_random_uuid () as id,
        CONCAT('Error:', description) AS message,
        text(id)  AS item_id
        FROM accounts WHERE status<>'OK')
+UNION
+SELECT gen_random_uuid () as id,
+       'WARN' as level,
+        'ACCOUNTS' as item,
+        'BALANCE' AS category,
+       CONCAT('Negarive balance:', accounts.description) AS message,
+       text(accounts.id)  AS item_id
+        FROM accounts
+            JOIN transactions on transactions.account_id=accounts.id
+            GROUP BY accounts.description, accounts.id
+            HAVING sum(amount_cents) < 0
+UNION
+SELECT gen_random_uuid () as id,
+       'WARN' as level,
+        'TRANSACTIONS' as item,
+        'CATEGORY_INVALID' AS category,
+        CONCAT('Invalid category:', category) AS message,
+       text(id) as item_id
+       FROM transactions WHERE category not in (select categories.code from categories)
+                           AND type<>'TRANSFER' AND type<>'INITIAL' AND booking_date>='2024-01-01'
 ORDER BY item ASC, item_id ASC;
