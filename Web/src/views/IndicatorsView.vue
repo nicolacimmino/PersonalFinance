d65b55e1e8dd0563701900ef8c76b356f7120ca6
Nicola Cimmino <nicola.cimmino@gmail.com>
2024-12-07 10:24:01 +0100

    Tweak

--- Web/src/views/IndicatorsView.vue
+++ Web/src/views/IndicatorsView.vue
@@ -64,10 +64,21 @@
       </div>
       <div class="kpi-derived-entry">
         <div class="kpi-description">
-          INP/(ESS+DST) (months)
+          INP/ESS+DST (months)
         </div>
         <div v-if="!privacy" class="kpi-value">
           {{ Math.floor(valueOfKpi('INP') / (164000 + 125000)) }}
+        </div>
+        <div v-else class="kpi-value">
+          ---
+        </div>
+      </div>
+      <div class="kpi-derived-entry">
+        <div class="kpi-description">
+          INV 6%/ESS+DST (months)
+        </div>
+        <div v-if="!privacy" class="kpi-value">
+          {{ Math.floor((0.06 * valueOfKpi('INV')) / (164000 + 125000)) }}
         </div>
         <div v-else class="kpi-value">
           ---
